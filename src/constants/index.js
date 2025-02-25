deb502f637f84439f611c9fe64cb8c1e5b743924
kamal-1030 <kamal.kayani@hotmail.com>
2025-02-25 15:07:04 +0100

    modified

--- src/constants/index.js
+++ src/constants/index.js
@@ -78,9 +78,3 @@ export const PROJECTS = [
     technologies: ["HTML", "CSS", "React", "Bootstrap"],
   }
 ];
-
-export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
-};
